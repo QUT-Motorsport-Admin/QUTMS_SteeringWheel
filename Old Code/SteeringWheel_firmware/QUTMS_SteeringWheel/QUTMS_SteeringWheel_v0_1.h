d9fa15bd86c7f569442542e9ebdfb202e1be25c7
Anthony Kellam <anthony@kaizengarage.com>
2018-07-31 11:39:29 +1000

    Latest steering wheel code from weekend work @ Anthony's - displays qutms splash then one screen set with 4 parameters including throttle percent

---
+++ Old Code/SteeringWheel_firmware/QUTMS_SteeringWheel/QUTMS_SteeringWheel_v0_1.h
@@ -0,0 +1,22 @@
+/*
+ * QUTMS_SteeringWheel_v0_1.h
+ *
+ * Created: 12/1/2015 1:14:40 PM
+ *  Author: julius
+ */ 
+
+
+#ifndef QUTMS_STEERINGWHEEL_V0_1_H_
+#define QUTMS_STEERINGWHEEL_V0_1_H_
+
+#ifndef F_CPU
+#define F_CPU 1000000UL
+#endif
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "OLED_v0_1.h"
+
+
+
+#endif /* QUTMS_STEERINGWHEEL_V0_1_H_ */
